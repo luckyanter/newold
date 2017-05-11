76cfe01c9e1042eb5104bc6fc9528d126af661a6
luckyanter <ahmedrezk@Ahmeds-MacBook-Air.local>
2017-05-11 19:53:40 -0400

    oo

--- app/public/js/add.js
+++ app/public/js/add.js
@@ -1,25 +1,96 @@
-
-$("#add-btn").on("click", function(event) {
-  event.preventDefault();
-
-  // Make a newmaterial object
-  var newmaterial = {
-    title: $("#title").val().trim(),
-    teacher: $("#teacher").val().trim(),
-    subject: $("#subject").val().trim(),
-    materials: $("#materials").val().trim()
-  };
-
-  // Send an AJAX POST-request with jQuery
-  $.post("/api/new", newmaterial)
-    .done(function(data) {
-      console.log(data);
-    });
-
-  // Empty each input box by replacing the value with an empty string
-  $("#title").val("");
-  $("#teacher").val("");
-  $("#subject").val("");
-  $("#materials").val("");
-
+$("#mySelect").on("change", function() {
+ var cat = $("#mySelect").val();
+ if(cat){
+    $.get("/api/" + cat, function(data){
+    console.log(data);
+    rendermaterials(data);
+  });
+  }
 });
+
+function rendermaterials(data) {
+  if (data.length !== 0) {
+
+    // $("#hey").empty();
+    // $("#hey").show();
+
+    for (var i = 0; i < data.length; i++) {
+
+      var div = $("<div>");
+
+      div.append("<h2>" + data[i].title + "</h2>");
+      div.append("<p>teacher: " + data[i].teacher + "</p>");
+      div.append("<p>subject: " + data[i].subject + "</p>");
+      div.append("<p>materials: " + data[i].materials + "</p>");
+      div.append("<button class='delete' data-id='" + data[i].id + "'>DELETE material</button>");
+
+      $("#hey").append(div);
+
+    }
+
+    // $(".delete").click(function() {
+
+    //   var info = {
+    //     id: $(this).attr("data-id")
+    //   };
+
+    //   $.post("/api/delete", info)
+    //     // On success, run the following code
+    //     .done(function(deldata) {
+    //       // Log the data we found
+    //       console.log(deldata);
+    //       console.log("Deleted Successfully!");
+    //     });
+    
+    //   $(this).closest("div").remove();
+
+    // });
+
+  }
+}
+
+
+
+
+
+// $("#subject-search-btn").on("click", function() {
+
+//   // Save the material they typed into the subject-search input
+//   var subjectSearched = $("#subject-search").val().trim();
+
+//   // Make an AJAX get request to our api, including the user's subject in the url
+//   $.get("/api/subject/" + subjectSearched, function(data) {
+
+//     console.log(data);
+//     // Call our rendermaterials function to add our materials to the page
+//     rendermaterials(data);
+
+//   });
+
+// });
+
+
+// $("#add-btn").on("click", function(event) {
+//   event.preventDefault();
+
+//   // Make a newmaterial object
+//   var newmaterial = {
+//     title: $("#title").val().trim(),
+//     teacher: $("#teacher").val().trim(),
+//     subject: $("#subject").val().trim(),
+//     materials: $("#materials").val().trim()
+//   };
+
+//   // Send an AJAX POST-request with jQuery
+//   $.post("/api/new", newmaterial)
+//     .done(function(data) {
+//       console.log(data);
+//     });
+
+//   // Empty each input box by replacing the value with an empty string
+//   $("#title").val("");
+//   $("#teacher").val("");
+//   $("#subject").val("");
+//   $("#materials").val("");
+
+// });
